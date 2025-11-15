6b6a956c651c4b71c677c4604d2f1f799a00c5c0
Rohit <61173483+Rohitkumar4321@users.noreply.github.com>
2025-11-15 16:49:20 +0530

    Format App.tsx for better readability

--- App.tsx
+++ App.tsx
@@ -1,1 +1,6 @@
-import KanbanBoard from './components/KanbanBoard/KanbanBoard';import data from './sample';export default function App(){return(<div className='p-6'><KanbanBoard columns={data.columns} tasks={data.tasks}/></div>);}
+import KanbanBoard from './components/KanbanBoard/KanbanBoard';
+import data from './sample';
+export default function App()
+{
+  return(<div className='p-6'><KanbanBoard columns={data.columns} tasks={data.tasks}/></div>);
+}
